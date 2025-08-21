bd8aebaa00dc11b2957ef49e9fa23f625cf3ba6c
Sajal17 <sajalmondal1730@gmail.com>
2025-08-21 21:53:58 +0530

    Update App.java

--- App.java
+++ App.java
@@ -16,8 +16,8 @@ public class App
         // Create factory and entity manager
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPU");
         EntityManager em = emf.createEntityManager();
-
-     /*   Student s=new Student();
+       // Create 
+        Student s=new Student();
         s.setId(101);
         s.setName("Ram");
         s.setLoc("Kolkata");
@@ -26,27 +26,28 @@ public class App
         em.getTransaction().begin();
         em.persist(s); // Persist (INSERT) student into DB
         em.getTransaction().commit(); // Commit transaction
-       */
+       
 
 
         // fetch
-        Student st=em.find(Student.class,101);
-        System.out.println(st);
+         Student st=em.find(Student.class,101);
+         System.out.println(st);
 
-        //Update
-//        em.getTransaction().begin();
-//        Student st1=em.find(Student.class,101);
-//        st1.setLoc("Habra");
-//        em.merge(st1);
-//        em.getTransaction().commit();
+          //Update
+          em.getTransaction().begin();
+          Student st1=em.find(Student.class,101);
+          st1.setLoc("Habra");
+          em.merge(st1);
+          em.getTransaction().commit();
 
-        //Delete
-//        em.getTransaction().begin();
-//        Student st2=em.find(Student.class,102);
-//        em.remove(st2);
-//        em.getTransaction().commit();
+          //Delete
+          em.getTransaction().begin();
+          Student st2=em.find(Student.class,102);
+          em.remove(st2);
+          em.getTransaction().commit();
 
         em.close();
         em.close();
     }
 }
+
